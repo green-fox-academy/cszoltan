421ca26fb48e3576594c765272bb486ad1258291
Zoli <cszoltan93@gmail.com>
2018-05-15 12:16:06 +0200

    student candy sum

---
+++ week-02/day-02/student-counter.ts
@@ -0,0 +1,28 @@
+'use strict';
+export {};
+
+let students: any[] = [
+  {name: 'Teodor', age: 3, candies: 2},
+  {name: 'Rezso', age: 9.5, candies: 2},
+  {name: 'Zsombor', age: 12, candies: 5},
+  {name: 'Aurel', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'Gerzson', age: 10, candies: 1},
+];
+
+function candyCount (input: any[]): number {
+  let out: number = 0;
+
+  input.forEach(function (value) {
+    out += value.candies;
+  });
+
+  return out;
+}
+
+console.log(candyCount(students));
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies

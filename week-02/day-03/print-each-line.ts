fc4121510485830b3f995575d2c8670b90a87454
Zoli <cszoltan93@gmail.com>
2018-05-16 15:34:45 +0200

    removed check exists

--- week-02/day-03/print-each-line.ts
+++ week-02/day-03/print-each-line.ts
@@ -6,12 +6,13 @@ let fs = require('fs');
 let charEncoding: string = 'utf8';
 
 try {
-  if (!fs.existsSync('my-file.txt')) {
+  /*if (!fs.existsSync('my-file.txt')) {
       throw new Error('Unable to read file: my-file.txt');
-  }    
+  }   */ 
   let myFile = fs.readFileSync('my-file.txt', charEncoding);
   console.log(myFile);
 }
 catch (e) {
-  console.log(e.message);
+  //console.log(e.message);
+  console.log('Unable to read file: my-file.txt');
 }

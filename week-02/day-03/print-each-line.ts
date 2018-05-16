e13c688b6d9f16e0747bc0375dfad1b2e23011af
Zoli <cszoltan93@gmail.com>
2018-05-16 10:21:25 +0200

    finally learned how to use required in ts

---
+++ week-02/day-03/print-each-line.ts
@@ -0,0 +1,17 @@
+'use strict';
+export {};
+
+declare function require(path: string): any;
+let fs = require('fs');
+let charEncoding: string = 'utf8';
+
+try {
+  if (!fs.existsSync('my-file.txt')) {
+      throw new Error('Unable to read file: my-file.txt');
+  }    
+  let myFile = fs.readFileSync('my-file.txt', charEncoding);
+  console.log(myFile);
+}
+catch (e) {
+  console.log(e.message);
+}

436b3c02e7f5a4deddd75b79d5bbbb21543616a8
Zoli <cszoltan93@gmail.com>
2018-05-17 09:42:59 +0200

    added export {} to all the codes

--- week-02/day-04/diagonals.ts
+++ week-02/day-04/diagonals.ts
@@ -4,21 +4,24 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
-// draw a red horizontal line to the canvas' middle.
-// draw a green vertical line to the canvas' middle.
-//600*400
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
 
 canvas.width = canvas.scrollWidth;
 canvas.height = canvas.scrollHeight;
 
 ctx.beginPath();
-ctx.moveTo(0, 200);
-ctx.lineTo(600, 200);
+
+ctx.moveTo(0, 0);
+ctx.lineTo(600, 400);
+
 ctx.strokeStyle = 'green';
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(300, 0);
-ctx.lineTo(300,400);
+
+ctx.moveTo(600, 0);
+ctx.lineTo(0, 400);
+
 ctx.strokeStyle = 'red';
 ctx.stroke();

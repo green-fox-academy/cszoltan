d219b2f134a47f74c3ca74873b80721764978fb9
Zoli <cszoltan93@gmail.com>
2018-05-17 11:59:53 +0200

    connecting dots with function

---
+++ week-02/day-04/connect-the-dots-ts.ts
@@ -0,0 +1,34 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a function that takes 1 parameter:
+// A list of [x, y] points
+// and connects them with green lines.
+// Connect these to get a box: [[10, 10], [290,  10], [290, 290], [10, 290]]
+// Connect these: [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70],
+// [120, 100], [85, 130], [50, 100]]
+
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
+
+let dots: number[][] = [[10, 10], [290,  10], [290, 290], [10, 290]];
+let dots2: number[][] = [[50, 100], [70, 70], [80, 90], [90, 90], [100, 70], [120, 100], [85, 130], [50, 100]];
+
+function connectDots(input: number[][]) {
+  for (let i: number = 0; i < input.length-1; i++) {
+    ctx.beginPath();
+    ctx.moveTo(input[i][0], input[i][1]);
+    ctx.lineTo(input[i+1][0], input[i+1][1]);
+    ctx.stroke();
+  }
+  ctx.beginPath();
+  ctx.moveTo(input[input.length-1][0], input[input.length-1][1]);
+  ctx.lineTo(input[0][0], input[0][1]);
+  ctx.stroke();
+}
+
+connectDots(dots);
+connectDots(dots2);

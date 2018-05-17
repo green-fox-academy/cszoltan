fd276ceb3b0e5c23aff05b4d1e66f364674b8fc6
Zoli <cszoltan93@gmail.com>
2018-05-17 15:08:40 +0200

    improved envelope code

--- week-02/day-05/envelope-star.ts
+++ week-02/day-05/envelope-star.ts
@@ -20,31 +20,24 @@ function drawLines(r: number[], b: number[], t: number[], l: number[], sw: numbe
   let m: number[] = [canvas.width / 2, canvas.height / 2];
   ctx.beginPath();
 
-  ctx.moveTo(t[0], t[1]);
+  ctx.moveTo(t[0], t[1]+sh);
   ctx.lineTo(m[0] + sw, m[1]);
 
-  ctx.moveTo(r[0], r[1]);
-  ctx.lineTo(m[0], m[1] - sh);
-
-  ctx.moveTo(r[0], r[1]);
+  ctx.moveTo(r[0]-sw, r[1]);
   ctx.lineTo(m[0], m[1] + sh);
 
-  ctx.moveTo(b[0], b[1]);
+  ctx.moveTo(b[0], b[1] - sh);
   ctx.lineTo(m[0] - sw, m[1]);
 
-  ctx.moveTo(l[0], l[1]);
+  ctx.moveTo(l[0]+sw, l[1]);
   ctx.lineTo(m[0], m[1] - sh);
 
   ctx.strokeStyle = 'green';
   ctx.stroke();
 }
 
-for (let i: number = 0; i < lines / 2; i++) {
+for (let i: number = 0; i < lines / 2 + 1; i++) {
   drawLines(right, bottom, topp, left, stepw[1], steph[1]);
-  topp[1] += steph[0];
-  right[0] -= stepw[0];
-  bottom[1] -= steph[0];
-  left[0] += stepw[0];
   stepw[1] = 0 + (canvas.width / lines) * i;
   steph[1] = 0 + (canvas.height / lines) * i;
 }

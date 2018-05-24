870ea0e7f554d2f33e16c9fa5bf693813d8c13cb
Zoli <cszoltan93@gmail.com>
2018-05-24 16:57:35 +0200

    square fractal draw with delay and hue pattern

---
+++ week-03/day-04/square-thingy.ts
@@ -0,0 +1,45 @@
+'use strict';
+//import {Circle} from "./circle";
+
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+
+canvas.width = canvas.scrollWidth;
+canvas.height = canvas.scrollHeight;
+
+class Square {
+  draw (x: number, y: number, l: number) {
+    ctx.beginPath();
+    ctx.strokeRect(x, y, l, l);
+  }
+}
+
+let rect: Square = new Square();
+
+function fractal (x: number, y: number, length: number): number {
+  if (length <= 1) {
+    return 1;
+  } else {
+    let ind: number = 1;
+    for (let i: number = x; i < x + length - 1; i += Math.round(length / 3)) {
+      let jnd: number = 1;
+      for (let j: number = y; j < y + length - 1; j += Math.round(length / 3)) {
+        if (ind === jnd && ind % 2 === 0) {
+          ctx.beginPath();
+          ctx.fillStyle = `hsl(${((i + j) / 18) % 360 + 180}, 100%, 50%)`;
+          ctx.fillRect(i, j, Math.round(length / 3), Math.round(length / 3));
+        }
+        setTimeout(function(){ 
+          fractal(i, j, Math.round(length / 3));
+        }, 2000);
+        jnd++;
+      }
+      ind++;
+    }
+    return /*drawBoxes(Math.round(length / 3), x + Math.round(length / 3), y + Math.round(length / 3))*/ 1;
+  }
+}
+
+fractal(0, 0, canvas.width);

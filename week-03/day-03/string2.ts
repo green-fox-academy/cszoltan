6e738db0fbec1afc3a047a1be374270fe8a682ad
Zoli <cszoltan93@gmail.com>
2018-05-24 09:49:22 +0200

    changed function names to represent the exercise better

--- week-03/day-03/string2.ts
+++ week-03/day-03/string2.ts
@@ -1,16 +1,16 @@
 'use strict';
 export {};
 
-function swapX (text: string): string {
+function removeX (text: string): string {
   if (text.length === 1 && text === 'x') {
     return '';
   } else if (text.length === 1 && text != 'x') {
     return text;
   } else if (text.charAt(text.length-1) === 'x') {
-    return swapX(text.slice(0, text.length - 1));
+    return removeX(text.slice(0, text.length - 1));
   } else {
-    return swapX(text.slice(0, text.length - 1)) + text.charAt(text.length-1);
+    return removeX(text.slice(0, text.length - 1)) + text.charAt(text.length-1);
   }
 }
 
-console.log(swapX('xasdfxghjklx'));
+console.log(removeX('xasdfxghjklx'));

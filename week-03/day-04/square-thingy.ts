b8099eba59d7241f026198c8d3538210e869608f
Zoli <cszoltan93@gmail.com>
2018-05-25 17:08:47 +0200

    made an improvement I noticed during my demo

--- week-03/day-04/square-thingy.ts
+++ week-03/day-04/square-thingy.ts
@@ -7,13 +7,24 @@ canvas.width = canvas.scrollWidth;
 canvas.height = canvas.scrollHeight;
 
 class Square {
-  draw (x: number, y: number, l: number) {
+  private x: number;
+  private y: number;
+  private l: number;
+
+  constructor (x: number, y: number, l: number) {
+    this.x = x;
+    this.y = y;
+    this.l = l;
+  }
+
+  draw (c: CanvasGradient) {
     ctx.beginPath();
-    ctx.strokeRect(x, y, l, l);
+    ctx.fillStyle = c;
+    ctx.fillRect(this.x, this.y, this.l, this.l);
   }
 }
-let start: number = Math.random() * 360;
-let rect: Square = new Square();
+
+let start: number = Math.floor(Math.random() * 360);
 
 function fractal (x: number, y: number, length: number): number {
   if (length <= 1) {
@@ -23,19 +34,24 @@ function fractal (x: number, y: number, length: number): number {
     for (let i: number = x; i < x + length - 1; i += Math.round(length / 3)) {
       let jnd: number = 1;
       for (let j: number = y; j < y + length - 1; j += Math.round(length / 3)) {
+        
         if (ind === jnd && ind % 2 === 0) {
-          ctx.beginPath();
-          ctx.fillStyle = `hsl(${((i + j) / 36) % 100 + start}, 100%, 50%)`;
-          ctx.fillRect(i, j, Math.round(length / 3), Math.round(length / 3));
-        }
+
+          let grad = ctx.createLinearGradient(i, j, i + Math.round(length / 3), j + Math.round(length / 3));
+          grad.addColorStop (0, `hsl(${Math.floor(((i + j) / 36)) % 100 + start}, 100%, 50%)`);
+          grad.addColorStop (1, `hsl(${Math.floor(((i + Math.round(length / 3) + j + Math.round(length / 3))  / 36)) % 100 + start}, 100%, 50%)`);
+      
+          let rect: Square = new Square(i, j, Math.round(length / 3));
+          rect.draw(grad);
+        } else {
         setTimeout(function(){ 
           fractal(i, j, Math.round(length / 3));
-        }, 2000);
+        }, 1000);}
         jnd++;
       }
       ind++;
     }
-    return 1;
+    return 0;
   }
 }
 

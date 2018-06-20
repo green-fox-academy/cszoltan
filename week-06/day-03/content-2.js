fbc812fb8166f6d7f70357ed904cc6b32182cf1e
Zoli <cszoltan93@gmail.com>
2018-06-20 11:41:28 +0200

    classList.add

---
+++ week-06/day-03/content-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let list = ['apple', 'banana', 'cat', 'dog'];
+
+for (let i = 0; i < list.length; i++) {
+  document.querySelectorAll('li')[i].innerText = list[i];
+}
+
+document.querySelector('ul').classList.add('coloring');

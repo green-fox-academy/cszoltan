8b854b9093b879c79822d66006cb3ec69c0c9403
Zoli <cszoltan93@gmail.com>
2018-06-20 15:14:52 +0200

    used the actual variable instead of saying document... every time

--- week-06/day-03/onclick.js
+++ week-06/day-03/onclick.js
@@ -5,9 +5,9 @@ let button = document.querySelector('button');
 button.onclick = () => {
   if (button.classList.contains('partyKiller')) {
     document.body.classList.remove('party');
-    document.querySelector('button').classList.remove('partyKiller');
+    button.classList.remove('partyKiller');
   } else {
     document.body.classList.add('party');
-    document.querySelector('button').classList.add('partyKiller');
+    button.classList.add('partyKiller');
   }
 };

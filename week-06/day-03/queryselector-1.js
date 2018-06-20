b3c44f910e9a6dc0d331879ec3ac8b8865581bce
Zoli <cszoltan93@gmail.com>
2018-06-20 11:15:46 +0200

    querySelector(All)

---
+++ week-06/day-03/queryselector-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let king = document.querySelector('#b325');
+console.log(king);
+
+let businessLamp = document.querySelectorAll('.asteroid.big');
+console.log(businessLamp[0]);
+console.log(businessLamp[1]);
+
+let conceitedKing = document.querySelectorAll('#b325, .b326');
+console.log(conceitedKing[0]);
+console.log(conceitedKing[1]);
+
+let noBusiness = document.querySelectorAll('#b325, .b326, .b329');
+console.log(noBusiness[0]);
+console.log(noBusiness[1]);
+console.log(noBusiness[2]);

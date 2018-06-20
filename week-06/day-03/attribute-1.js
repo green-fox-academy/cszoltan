e14d0cc34f97d264b86275d41e327348fca71ecc
Zoli <cszoltan93@gmail.com>
2018-06-20 11:52:55 +0200

    (g/s)etAttritbute

---
+++ week-06/day-03/attribute-1.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log(document.querySelector('img').getAttribute('src'));
+document.querySelector('img').setAttribute('src', 'https://images.earthtouchnews.com/media/1949490/rhino_mating_2_2017-08-04.jpg');
+document.querySelector('a').setAttribute('href', 'https://www.greenfoxacademy.com/');
+document.querySelector('.this-one').setAttribute('disabled', 'true');

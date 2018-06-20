a96fed808643ac58e456c3a2fe1416558b534157
Zoli <cszoltan93@gmail.com>
2018-06-20 11:27:41 +0200

    innerText and innerHTML

---
+++ week-06/day-03/content-1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('.apple').innerText = document.querySelector('.animals').innerText;
+document.querySelector('.balloon').innerText = document.querySelector('.animals').innerText;
+document.querySelector('.cat').innerText = document.querySelector('.animals').innerText;
+
+document.querySelector('.apple').innerHTML = document.querySelector('.animals').innerHTML;
+document.querySelector('.balloon').innerHTML = document.querySelector('.animals').innerHTML;
+document.querySelector('.cat').innerHTML = document.querySelector('.animals').innerHTML;

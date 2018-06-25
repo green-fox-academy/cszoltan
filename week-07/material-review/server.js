065ac24944e07a1ef11cbbca4267f313afd6e6ce
Zoli <cszoltan93@gmail.com>
2018-06-25 11:29:08 +0200

    review

---
+++ week-07/material-review/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const cars = [
+  {id: 1, brand: 'idk', type: 'wat'},
+  {id: 2, brand: 'idk2', type: 'wat2'},
+  {id: 3, brand: 'idk3', type: 'wat3'}
+];
+
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+  res.json({
+    message: 'pong',
+  });
+});
+
+app.get('/api/cars', (req, res) => {
+  res.json({
+    cars: cars,
+  });
+});
+
+app.post('/api/cars', (req, res) => {
+  cars.push({
+    id: cars.length + 1,
+    brand: req.body.brand,
+    type: req.body.type
+  });
+  res.json({
+    message: 'fuck off',
+  });
+});
+
+app.get('/idk', (req, res) => {
+  res.json({
+    message: "new useless shit",
+  });
+});
+
+app.delete('/api/cars', (req, res) => {
+  cars.pop();
+  res.json({
+    message: 'deleted the last car',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT);
+});

9a481cd9d70992f67971cc3dcce4467cb37eb2c0
Zoli <cszoltan93@gmail.com>
2018-06-26 11:09:40 +0200

    multiple set timeout

---
+++ week-07/day-02/multiple-timeout.js
@@ -0,0 +1,12 @@
+'use strict';
+
+setTimeout(() => console.log('apple'), 0);
+setTimeout(() => console.log('pear'), 1000);
+setTimeout(() => console.log('melon'), 2000);
+setTimeout(() => console.log('grapes'), 3000);
+
+// Write a program that prints the following fruits:
+// apple -> immediately
+// pear -> after 1 seconds
+// melon -> after 3 seconds
+// grapes -> after 5 seconds
